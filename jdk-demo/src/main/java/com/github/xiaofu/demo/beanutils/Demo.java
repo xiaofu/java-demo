fd634342781d2801343e55007a64abc840e99a31
xiaofu <wfygowxf1@163.com>
2016-09-20 22:45:09 +0800

    demo

--- jdk-demo/src/main/java/com/github/xiaofu/demo/beanutils/Demo.java
+++ jdk-demo/src/main/java/com/github/xiaofu/demo/beanutils/Demo.java
@@ -2,21 +2,26 @@ package com.github.xiaofu.demo.beanutils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.beanutils.BasicDynaClass;
 import org.apache.commons.beanutils.DynaBean;
 import org.apache.commons.beanutils.DynaProperty;
+import org.apache.commons.beanutils.LazyDynaBean;
+import org.apache.commons.beanutils.LazyDynaList;
+import org.apache.commons.beanutils.LazyDynaMap;
 import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.beanutils.WrapDynaBean;
 
 public class Demo {
 
-	public static void standard() throws IllegalAccessException,
-			InvocationTargetException, NoSuchMethodException {
+	public static void standard() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 		Employee employee = new Employee();
 		// simple property
 		PropertyUtils.setSimpleProperty(employee, "firstName", "ok");
-		System.out.println(PropertyUtils.getSimpleProperty(employee,
-				"firstName"));
+		System.out.println(PropertyUtils.getSimpleProperty(employee, "firstName"));
 		// indexed property
 		int index = 0;
 		Employee subordinate = new Employee();
@@ -24,12 +29,9 @@ public class Demo {
 		subordinate.setFirstName("sec");
 		String name = "subordinate[" + index + "]";
 		PropertyUtils.setIndexedProperty(employee, name, subordinate);
-		PropertyUtils.setIndexedProperty(employee, "subordinate", 1,
-				subordinate);
-		subordinate = (Employee) PropertyUtils.getIndexedProperty(employee,
-				name);
-		subordinate = (Employee) PropertyUtils.getIndexedProperty(employee,
-				"subordinate", 1);
+		PropertyUtils.setIndexedProperty(employee, "subordinate", 1, subordinate);
+		subordinate = (Employee) PropertyUtils.getIndexedProperty(employee, name);
+		subordinate = (Employee) PropertyUtils.getIndexedProperty(employee, "subordinate", 1);
 		System.out.println(subordinate.getFirstName());
 		// mapped property
 		Employee.Address address = new Employee().new Address();
@@ -37,28 +39,75 @@ public class Demo {
 		// Nested Property
 		PropertyUtils.getNestedProperty(employee, "address(home).city");
 		// 任何类型的属性操作
-		PropertyUtils
-				.getProperty(employee, "subordinate[0].address(home).city");
+		PropertyUtils.getProperty(employee, "subordinate[0].address(home).city");
 	}
 
-	public static void dynamic() throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
-		DynaProperty[] props = new DynaProperty[] {
-				new DynaProperty("address", java.util.Map.class),
-				new DynaProperty("subordinate", Employee[].class),
-				new DynaProperty("firstName", String.class),
+	public static void BasicDynaBeanTest()
+			throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+		DynaProperty[] props = new DynaProperty[] { new DynaProperty("address", java.util.Map.class),
+				new DynaProperty("subordinate", Employee[].class), new DynaProperty("firstName", String.class),
 				new DynaProperty("lastName", String.class) };
 		BasicDynaClass dynaClass = new BasicDynaClass("employee", null, props);
-		DynaBean employee= dynaClass.newInstance();
-		 employee.set("address", new HashMap());
-	     employee.set("subordinate", new  Employee[0]);
-	     employee.set("firstName", "Fred");
-	     employee.set("lastName", "Flintstone");
-	     PropertyUtils.setProperty(employee, "firstName", "ok");
-	     System.out.println(PropertyUtils.getProperty(employee, "firstName"));
+		DynaBean employee = dynaClass.newInstance();
+		employee.set("address", new HashMap());
+		employee.set("subordinate", new Employee[0]);
+		employee.set("firstName", "Fred");
+		employee.set("lastName", "Flintstone");
+		PropertyUtils.setProperty(employee, "firstName", "ok");
+		System.out.println(PropertyUtils.getProperty(employee, "firstName"));
+		// throw exception
+		// System.out.println(PropertyUtils.getProperty(employee, "aa"));
+
 	}
 
-	public static void main(String[] args) throws IllegalAccessException,
-			InvocationTargetException, NoSuchMethodException, InstantiationException {
-		dynamic();
+	public static void WrapDynaBeanTest() {
+		Employee employee = new Employee();
+		WrapDynaBean bean = new WrapDynaBean(employee);
+		bean.set("firstName", "Fred");
+		bean.set("lastName", "Flintstone");
+		System.out.println(bean.get("lastName"));
+		// throw exception
+		System.out.println(bean.get("aa"));
+	}
+
+	public static void LazyDynaBeanTest() {
+		LazyDynaBean dynaBean = new LazyDynaBean();
+
+		dynaBean.set("foo", "bar"); // simple
+
+		dynaBean.set("customer", "title", "Mr"); // mapped
+		dynaBean.set("customer", "surname", "Smith"); // mapped
+
+		dynaBean.set("address", 0, "addressLine1"); // indexed
+		dynaBean.set("address", 1, "addressLine2"); // indexed
+		dynaBean.set("address", 2, "addressLine3"); // indexed
+	}
+	/**
+	 * 一般主要用来读写simple,mapped and indexed 不关心
+	 */
+	public static void LazyDynaMapTest()
+	{
+		LazyDynaMap dynaBean = new LazyDynaMap();        // create DynaBean
+
+	     dynaBean.set("foo", "bar");                   // simple
+	     dynaBean.set("customer", "title", "Mr");      // mapped
+	     dynaBean.set("address", 0, "addressLine1");     // indexed
+
+	     Map myMap = dynaBean.getMap()  ;              // retrieve the Map
+	     
+	     
+	    // DynaBean dynaBean = new LazyDynaMap(myMap);  // wrap Map in DynaBean
+	     // dynaBean.set("foo", "bar");                  // set properties
+	}
+	
+	public static void LazyDynaListTest()
+	{
+		TreeMap[] myArray = new TreeMap[2]; // your Map[]
+			    List lazyList = new LazyDynaList(myArray);
+	}
+	public static void main(String[] args)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+		BasicDynaBeanTest();
+		WrapDynaBeanTest();
 	}
 }
