3ae795083ce03baefe5b6b7347ba7f26c69b44a0
xiaofu <wfygowxf1@163.com>
2016-11-29 16:25:51 +0800

    demo

--- hadoop-demo/src/main/java/com/github/xiaofu/demo/hadoop/Demo.java
+++ hadoop-demo/src/main/java/com/github/xiaofu/demo/hadoop/Demo.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.DFSClient;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
 import org.apache.hadoop.hdfs.protocol.LocatedBlock;
 import org.apache.hadoop.hdfs.protocol.LocatedBlocks;
@@ -14,8 +15,9 @@ import org.apache.hadoop.hdfs.protocol.LocatedBlocks;
 public class Demo {
 	public static void main(String[] args) throws IOException {
 		Path path = new Path("/vipcloud/journal/down_infos/current_vnamenode_2016-11-09");
-		FileSystem fs = FileSystem.get(new Configuration());
-		DFSClient client = new DFSClient(fs.getConf());
+		DistributedFileSystem fs = (DistributedFileSystem)  FileSystem.get(new Configuration());
+		
+		DFSClient client =fs.getClient();
 		LocatedBlocks locateBlocks = client.getLocatedBlocks(path.toUri().toString(), 0);
 		for (LocatedBlock locateBlock : locateBlocks.getLocatedBlocks()) {
 			for (DatanodeInfo datanodeInfo : locateBlock.getLocations()) {
@@ -26,6 +28,7 @@ public class Demo {
 		for (BlockLocation blockLocation : client.getBlockLocations(path.toUri().toString(), 0, fs.getLength(path))) {
 			for (String host : blockLocation.getHosts()) {
 				System.out.println("host:" + host);
+				
 			}
 			for (String name : blockLocation.getNames()) {
 				System.out.println("host:" + name);
