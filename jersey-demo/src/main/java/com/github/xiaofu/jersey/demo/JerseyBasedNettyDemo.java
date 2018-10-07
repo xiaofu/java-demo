31d928eb40ba7d9878e0161e7dbe6bd9b194b92e
xiaofu <wfygowxf1@163.com>
2018-10-08 00:19:23 +0800

    jersey demo

---
+++ jersey-demo/src/main/java/com/github/xiaofu/jersey/demo/JerseyBasedNettyDemo.java
@@ -0,0 +1,21 @@
+package com.github.xiaofu.jersey.demo;
+
+ 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.ProcessingException;
+
+import io.netty.channel.Channel;
+
+import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class JerseyBasedNettyDemo {
+
+	public static void main(String[] args) throws ProcessingException, URISyntaxException {
+		 final Channel server = NettyHttpContainerProvider.createHttp2Server(new URI("http://localhost:8080/"), new ResourceConfig(HelloWorldResource.class), null);
+
+	}
+
+}
