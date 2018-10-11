858b2d06d5f0d11eb1f2773e6c44306fde131438
xiaofu <wfygowxf1@163.com>
2018-10-11 23:11:59 +0800

    jersey demo

--- jersey-demo/src/main/java/com/github/xiaofu/jersey/demo/JerseyBasedNettyDemo.java
+++ jersey-demo/src/main/java/com/github/xiaofu/jersey/demo/JerseyBasedNettyDemo.java
@@ -8,13 +8,17 @@ import javax.ws.rs.ProcessingException;
 
 import io.netty.channel.Channel;
 
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class JerseyBasedNettyDemo {
 
 	public static void main(String[] args) throws ProcessingException, URISyntaxException {
-		 final Channel server = NettyHttpContainerProvider.createServer(new URI("http://localhost:8080/"), new ResourceConfig(HelloWorldResource.class), false);
+		ResourceConfig config =new ResourceConfig(); 
+		config.register(HelloWorldResource.class);
+		config.register(LoggingFeature.class);
+		final Channel server = NettyHttpContainerProvider.createServer(new URI("http://localhost:8080/"), config, false);
 
 	}
 
