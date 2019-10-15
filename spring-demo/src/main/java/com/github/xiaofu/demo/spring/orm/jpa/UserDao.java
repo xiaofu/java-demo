7b0b486d5dd9bfd615367a9cc79e3661ef6b464c
flh <wfygowxf1@163.com>
2019-10-15 22:35:53 +0800

    spring ioc demo

--- spring-demo/src/main/java/com/github/xiaofu/demo/spring/orm/jpa/UserDao.java
+++
@@ -1,5 +0,0 @@
-package com.github.xiaofu.demo.spring.orm.jpa;
-
-public interface UserDao {
-	public long save(AccountInfo accountInfo); 
-}
--- spring-demo/src/main/java/com/github/xiaofu/demo/spring/orm/jpa/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.github.xiaofu.demo.spring.orm.jpa;
-
-public interface UserService { 
-	 public long createNewAccount(String user, String pwd, Integer init); 
-	 }
